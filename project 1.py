a55971a0cc66407db9df9a35a031ddeb2b08d0cb
1199692 <56451987+1199692@users.noreply.github.com>
2019-12-17 20:29:31 +0100

    Update project 1.py

--- project 1.py
+++ project 1.py
@@ -1,69 +1,65 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+# find out if the word 'dog' is in a string
+class TT:
 
-This is a temporary script file.
-"""
-global moves
-def startgame():
-    a=(input('welcome! Do you want to play Game now '))
-    if a.lower()=='yes' or'yup':
-        b=(input('which team you do you want to choose x or o '))
-        if b.lower()=='x':
-            Iboard()
-            Cxboard()
-        elif b.lower()=='o':
-            Iboard()
-            Coboard()
-        else:
-            print('please select out of x and o only')
+    def startgame():
+        a=(input('welcome! Do you want to play Game now '))
+        if a.lower()=='yes' or'yup':
+            b=(input('which team you do you want to choose x or o '))
+            if b.lower()=='x':
+                TT.Iboard()
+                TT.Cxboard()
+            elif b.lower()=='o':
+                TT.Iboard()
+                TT.Coboard()
+            else:
+                print('please select out of x and o only')
             pass
-    elif a.lower()=='no':
-        print('Thanks for playing!Come back again')
-    else:
-        print('please provide your correct input(yes/No)')
-        startgame()
-def Iboard():
-    moves = [' ', ' ',' ',' ', ' ',' ',' ', ' ',' ']
-    board = "|{}|{}|{}|\n|{}|{}|{}|\n|{}|{}|{}|\n"
-    print(board.format(*moves))
-
-def Cxboard():
-    moves = [' ', ' ',' ',' ', ' ',' ',' ', ' ',' ']
-    board = "|{}|{}|{}|\n|{}|{}|{}|\n|{}|{}|{}|\n"
-    j=int(input('Team x-where do you want to put'))
-    moves[j-1]='x'
-    print(board.format(*moves))
-    for i in range(0,10):
-        if (moves[i]== moves[i+1]== moves[i+2]) or (moves[i] == moves[i+2] == moves[i+4]):
-            print('you won!')
-            c=input('do you want to play again')
-            if c.lower()=='yes':  
-                startgame()
-            elif c.lower()=='no':
-                print('Thanks for playing!Come back again')
-            else:
-                print('please provide your correct input(yes/No)')
-                startgame()
+        elif a.lower()=='no':
+            print('Thanks for playing!Come back again')
         else:
-            Coboard()       
+            print('please provide your correct input(yes/No)')
+            TT.startgame()
+    def Iboard():
+        moves = [' ', ' ',' ',' ', ' ',' ',' ', ' ',' ']
+        board = "|{}|{}|{}|\n|{}|{}|{}|\n|{}|{}|{}|\n"
+        print(board.format(*moves))
+
+    def Cxboard():
+        moves = [' ', ' ',' ',' ', ' ',' ',' ', ' ',' ']
+        board = "|{}|{}|{}|\n|{}|{}|{}|\n|{}|{}|{}|\n"
+        j=int(input('Team x-where do you want to put'))
+        moves[j-1]='x'
+        print(board.format(*moves))     
+        for i in range(0,10):
+            if (moves[i]== moves[i+1]== moves[i+2]) or (moves[i] == moves[i+2] == moves[i+4]):
+                print('you won!')
+                c=input('do you want to play again')
+                if c.lower()=='yes':  
+                    TT.startgame()
+                elif c.lower()=='no':
+                    print('Thanks for playing!Come back again')
+                else:
+                    print('please provide your correct input(yes/No)')
+                    TT.startgame()
+            else:
+                TT.Coboard()       
     
-def Coboard():
-    k=int(input('Team y-where do you want to put'))
-   Cxboard.moves[k- 1]='o'
-    print(board.format(*(moves)))
-    for i in range(0,10):
-        if (x.moves[i]== x.moves[i+1]== x.moves[i+2]) or (x.moves[i] == x.moves[i+2] == x.moves[i+4]):
-            print('you won!')
-            d=input('do you want to play again')
-            if d.lower()=='yes':
-                startgame()
-            elif d.lower()=='no':
-                print('Thanks for playing!Come back again')
+    def Coboard():
+        j=int(input('Team y-where do you want to put'))
+        moves[j - 1]='o'
+        print(board.format(*(moves)))
+        for i in range(0,10):
+            if (moves[i]== moves[i+1]== moves[i+2]) or (moves[i] == moves[i+2] == moves[i+4]):
+                print('you won!')
+                d=input('do you want to play again')
+                if d.lower()=='yes':
+                    TT.startgame()
+                elif d.lower()=='no':
+                    print('Thanks for playing!Come back again')
+                else:
+                    print('please provide your correct input(yes/No)')
+                    TT.startgame()
             else:
-                print('please provide your correct input(yes/No)')
-                startgame()
-        else:
-            Cxboard()
+                TT.Cxboard()
 
-startgame()
+TT.startgame()
